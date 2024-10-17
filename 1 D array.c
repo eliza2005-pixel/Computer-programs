5cefe1ae527f3864834450d295b9b35166d0d7cf
eliza2005-pixel <elizamulee316@gmail.com>
2024-10-17 15:45:29 +0300

    Add files via upload

---
+++ 1 D array.c
@@ -0,0 +1,14 @@
+/*
+Author:eliza mulee 
+Reg no:CT102\G\22239/24
+*/
+//Arrays in c -1D array
+#include<stdio.h>
+int main(){
+int i;
+int marks[5]={50,90,60,70,40};
+for(i=0;i<5;i++){
+    printf("marks[%d]=%d\n",i,marks[i]);
+    }
+    return 0;
+}
